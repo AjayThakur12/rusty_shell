ea5bb558cfe776636e55a891e5422c8eb7b66700
Ajay Singh <ajaysij@amazon.com>
2024-06-29 17:41:26 +0530

    added non-supported cmd functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use std::io::{self, Stdin, Write};
 
+#[allow(unused)]
+enum Commands {
+    ECHO(String),
+    LS(String),
+    MKDIR(String),
+}
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
@@ -7,4 +13,11 @@ fn main() {
     let stdin: Stdin = io::stdin();
     let mut input: String = String::new();
     stdin.read_line(&mut input).unwrap();
+    // print!("{}", input);
+    match input.as_str() {
+        "ECHO" => println!("This is an ECHO."),
+        "LS" => println!("This is an LS."),
+        "MKDIR" => println!("This is a MKDIR."),
+        _ => println!("{} : command not found", input.trim()),
+    }
 }
